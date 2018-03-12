38af2f26897bb890ff9a6d01f80c567a3b16bb2b
abarnes26 <abarnes26@yahoo.com>
2018-03-11 22:12:38 -0600

    completed

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  debugger;
+  $('#style_editor').submit(function(event) {
+    event.preventDefault();
+    var selector = $(this.selector).val()
+    var property = $(this.property).val()
+    var value = $(this.value).val()
+    $(selector).css(property, value)
+  })
 });
